42a6638bb9da410c3ae5fceaba3dd5da3aca7cc5
artembigdata <64332640+artembigdata@users.noreply.github.com>
2021-01-05 18:26:36 +0500

    2

--- task_1.py
+++ task_1.py
@@ -1,10 +1,25 @@
-def my_fun(s_1 = 5, s_2 = 2):
-    s_1 = int(input("введите число_1: "))
-    s_2 = int(input("введите число_2:  "))
+def my_fun(s_1: object, s_2: object) -> object:
 
-    sub = s_1 / s_2
-    print(f"Sub: {sub}")
-my_fun(25, 5)
+  s_1 = int(input("введите число_1: "))
+  s_2 = int(input("введите число_2:  "))
+
+  sub = s_1 / s_2
+  print(f"{sub}")
+
+ my_fun(s_1, s_2)
+
+#если отдельно не написать работать не будет№
+
+
+
+
+
+
+
+
+
+
+
 
 
 
