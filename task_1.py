0b6417f50d295b89e6df270f31fda5e7e7e9879d
artembigdata <64332640+artembigdata@users.noreply.github.com>
2021-01-05 17:38:21 +0500

    1

---
+++ task_1.py
@@ -0,0 +1,16 @@
+def my_fun(s_1 = 5, s_2 = 2):
+    s_1 = int(input("введите число_1: "))
+    s_2 = int(input("введите число_2:  "))
+
+    sub = s_1 / s_2
+    print(f"Sub: {sub}")
+my_fun(25, 5)
+
+
+
+
+
+
+
+
+
